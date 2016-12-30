ff517604d3bf0f562e0f5ab909b99251fce9cc01
vfa-dangnh <dangnh@vitalify.asia>
2016-12-30 17:13:04 +0700

    First commit

---
+++ app/src/main/java/com/haidangkf/nicechat/RegisterActivity.java
@@ -0,0 +1,124 @@
+package com.haidangkf.nicechat;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.firebase.client.Firebase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by dangnguyen on 12/28/16.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText username, password, confirmPassword;
+    Button registerButton;
+    String user, pass, confirmPass;
+    TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+        registerButton = (Button) findViewById(R.id.registerButton);
+        login = (TextView) findViewById(R.id.login);
+
+        Firebase.setAndroidContext(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = username.getText().toString();
+                pass = password.getText().toString();
+                confirmPass = confirmPassword.getText().toString();
+
+                if (user.equals("")) {
+                    username.setError("can't be blank");
+                } else if (pass.equals("")) {
+                    password.setError("can't be blank");
+                } else if (confirmPass.equals("")) {
+                    confirmPassword.setError("can't be blank");
+                } else if (!pass.equals(confirmPass)) {
+                    Toast.makeText(RegisterActivity.this, "Password does not match !", Toast.LENGTH_SHORT).show();
+                } else if (!user.matches("[A-Za-z0-9]+")) {
+                    username.setError("only alphabet or number allowed");
+                } else if (user.length() < 5) {
+                    username.setError("at least 5 characters long");
+                } else if (pass.length() < 5) {
+                    password.setError("at least 5 characters long");
+                } else {
+                    final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
+                    pd.setMessage("Loading...");
+                    pd.show();
+
+                    String url = "https://android-chat-app-e711d.firebaseio.com/users.json";
+
+                    StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String s) {
+                            Firebase reference = new Firebase("https://android-chat-app-e711d.firebaseio.com/users");
+
+                            if (s.equals("null")) {
+                                reference.child(user).child("password").setValue(pass);
+                                Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                            } else {
+                                try {
+                                    JSONObject obj = new JSONObject(s);
+
+                                    if (!obj.has(user)) {
+                                        reference.child(user).child("password").setValue(pass);
+                                        Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "username already exists", Toast.LENGTH_LONG).show();
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            pd.dismiss();
+                        }
+
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            System.out.println("" + volleyError);
+                            pd.dismiss();
+                        }
+                    });
+
+                    RequestQueue rQueue = Volley.newRequestQueue(RegisterActivity.this);
+                    rQueue.add(request);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/haidangkf/nicechat/UserDetails.java
@@ -0,0 +1,11 @@
+package com.haidangkf.nicechat;
+
+/**
+ * Created by dangnguyen on 12/28/16.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
